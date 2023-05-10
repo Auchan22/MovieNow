cf950fbf340ed09596f0cacfbb0282c51a20dba2
Agustin <agustinsurila@gmail.com>
2023-05-10 20:34:23 -0300

    Comenzando pagina de movie

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,7 +1,27 @@
+"use client"
+
+import { useMovie } from "@/hooks"
+
 interface Props {
   params: { id: string }
 }
 
 export default function MoviePage({ params }: Props) {
-  return <h1>hola desde pagina de peli con id: {params.id}</h1>
+  const { data, isLoading } = useMovie(params.id).movieQuery
+
+  if (isLoading) return <h1>Cargando</h1>
+  return (
+    <div className="h-screen w-screen">
+      <div className="relative h-[70%] bg-gradient-to-b from-red-500 shadow-md">
+        <img
+          src={`http://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          width="100%"
+          className="absolute max-h-[100%] "
+        />
+        <h1 className="absolute top-0 min-h-full w-full backdrop-blur-sm">
+          {data?.title}
+        </h1>
+      </div>
+    </div>
+  )
 }
