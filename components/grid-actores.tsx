315535efca0b7c98b3a77c480d51f1408715a65e
Agustin <agustinsurila@gmail.com>
2023-05-22 20:52:09 -0300

    Arreglo de grid de actores, limitando la cantidad a mostrar

--- components/grid-actores.tsx
+++ components/grid-actores.tsx
@@ -7,10 +7,9 @@ interface Props {
 }
 
 export function GridActores({ data }: Props) {
-  const arr = data?.cast.filter(
-    (e) => e.known_for_department === Department.Acting
-  )
-  console.log(arr)
+  const arr = data?.cast
+    .filter((e) => e.known_for_department === Department.Acting)
+    .slice(0, 18)
   return (
     <div className="mx-8 flex max-w-max flex-row flex-wrap items-start justify-center gap-5  py-4">
       {arr?.map((el) => (
